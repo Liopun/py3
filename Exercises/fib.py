8267658909dc76cf4c46cf5ca1cb410811f1eeee
Captone Habiyaremye <hcaptone@gmail.com>
2020-01-13 23:06:22 +0000

    fib

---
+++ Exercises/fib.py
@@ -0,0 +1,13 @@
+# Fibonacci Sequence
+def fib(x):
+  if (x < 0):
+    print("Incorrect Input!")
+  elif (x == 0):
+    return 0
+  elif (x == 1):
+    return 1
+  else:
+    return (fib(x-1) + fib(x-2))
+
+fib_input = int(input("\nEnter A Number: "))
+print(fib(fib_input))
