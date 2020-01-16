6396f1239f1b1e9e161817ff6de5444f8d829d27
Captone Habiyaremye <hcaptone@gmail.com>
2020-01-16 12:56:21 -0500

    Updated array del

--- exercises/array_del.py
+++ exercises/array_del.py
@@ -2,12 +2,22 @@ import array
 import random
 
 
+def reverse_array(new_arr):
+    i_start, i_end = 0, (len(new_arr) - 1)
+    for x in new_arr:
+        new_arr[i_start] = new_arr[i_end]
+        i_start += 1
+        i_end -= 1
+    return new_arr
+
+
 def array_del(arr, index):
     new_arr = []
     for x in arr:
         if x != arr[index]:
             new_arr.append(x)
-    print("Old Array: %s \nNew Array (No index %d): %s" % (arr, index, new_arr))
+    print("Old Array: %s \nNew Array (Remove index %d): %s \nReversed New Array: %s" % (
+    arr, index, new_arr, reverse_array(new_arr)))
 
 
 array_int = []
