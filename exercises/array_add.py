d2d9b69e34a17a6842610b7b28cfd85c38f6c092
Captone Habiyaremye <hcaptone@gmail.com>
2020-01-18 12:17:26 -0500

    fixed array addition

--- exercises/array_add.py
+++ exercises/array_add.py
@@ -1,21 +1,18 @@
 import random
-import numpy as np
+import array
 
-
-arr1, arr2 = np.array([]), np.array([])
+arr1, arr2 = array.array('i', []), array.array('i', [])
 
 
 def adds(arg1, arg2):
-    new_arr = np.array([])
-    for x in np.arange(arg1.size):
+    new_arr = []
+    for x in range(len(arg1)):
         new_arr.append(arg1[x] + arg2[x])
     return new_arr
 
 
 while len(arr1) < 5:
-    arr1.append()
-    arr1 = np.append(random.randint(1, 100))
-    arr2.append()
-    arr2 = np.append(random.randint(1, 100))
+    arr1.append(random.randint(1, 100))
+    arr2.append(random.randint(1, 100))
 
 print("Arr 1: %s \nArr 2: %s \nAddition: %s" %(arr1, arr2, adds(arr1, arr2)))
