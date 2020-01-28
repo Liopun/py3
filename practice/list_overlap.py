db30864e6f277e110a299748643f0abe3d3ff286
Captone Habiyaremye <hcaptone@gmail.com>
2020-01-28 14:40:56 -0500

    Update list overlap

--- practice/list_overlap.py
+++ practice/list_overlap.py
@@ -1,4 +1,5 @@
 import random
+
 # List Overlap
 # Generate 2 numbers for array size
 size1, size2 = random.randint(1, 10), random.randint(1, 10)
@@ -8,10 +9,10 @@ arr1 = random.sample(range(1, 100), size1)
 arr2 = random.sample(range(1, 100), size2)
 
 
-def compare(arr1, arr2):
+def compare(a1, a2):
     final = []
-    for x in arr1:
-        for y in arr2:
+    for x in a1:
+        for y in a2:
             if x == y and x not in final:
                 final.append(x)
                 continue
@@ -20,4 +21,9 @@ def compare(arr1, arr2):
 
 
 print("list 1: %s\nlist 2: %s" % (arr1, arr2))
-print("Joined List (no duplicates): %s", compare(arr1, arr2))
+print("Joined List (no duplicates): %s" % compare(arr1, arr2))
+
+# Advance way
+c = [num for num in arr1 if num in arr2]
+print("Joined List (no duplicates):", c)
+
